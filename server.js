e9fb6a23c8bd0934c4b9c1e763b9e9882400ac1f
Alexander Schelchere <ascellie@gmail.com>
2019-08-27 16:59:01 -0400

    use json file to show data on api routes

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+// Get the word bank scraper
+const WorldBankScraper = require('./app/WorldBankScraper');
+// get url
+const { worldBankUrl } = require('./config');
 // Dependencies
 const express = require('express');
-
 // use express
 const app = express();
+// Get json data
+const data = require('./json/worldbank-0.json');
 
 // set port
 const port = process.env.PORT || 3000;
@@ -10,13 +15,14 @@ app.set('port', port);
 
 // GET all forests
 app.get('/api/forests/', (req, res) => {
-  res.json({ test: "Hello from '/api/forests/' route." });
+  res.json({ data });
 });
 
 // GET one forest
 app.get('/api/forests/:id', (req, res) => {
   const id = req.params['id'];
-  res.json({ test: `Hello from '/api/forests/${id}' route.` });
+  const country = data.find(country => country.id == id);
+  res.json({ country });
 });
 
 // Listen, linda.
