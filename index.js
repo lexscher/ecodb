d5bf55cfb909c7720e7e6fb433f088055926feea
Alexander Schelchere <ascellie@gmail.com>
2019-08-27 17:50:10 -0400

    cleanup

--- index.js
+++ index.js
@@ -13,12 +13,16 @@ const data = require('./json/worldbank-0.json');
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
-// GET all forests
-app.get('/api/countries/', (req, res) => {
+// Home
+app.get('/', (req, res) => {
+  res.json({ data });
+});
+// GET all countries
+app.get('/api/countries', (req, res) => {
   res.json({ data });
 });
 
-// GET one forest
+// GET one country
 app.get('/api/countries/:id', (req, res) => {
   const id = req.params['id'];
   const country = data.find(country => country.id == id);
